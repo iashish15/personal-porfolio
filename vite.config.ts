c7ed1b7d4333a865d818587cf911e9fc3f7b79eb
itsashish15 <ashish@birbals.com>
2026-05-10 20:29:38 +0530

    Fix GitHub Pages assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personal-porfolio/",
-  server: {
-    port: 5173,
-    host: "0.0.0.0",
-  },
 });
