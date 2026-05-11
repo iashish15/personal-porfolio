66ec91c25755d6d3fadb31a90526f2df8fcf392a
itsashish15 <ashish@birbals.com>
2026-05-11 10:47:57 +0530

    fix deploy asset paths and add gitignore for temp files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+function getBase() {
+  if (process.env.VITE_BASE_URL) {
+    return process.env.VITE_BASE_URL;
+  }
+
+  return "./";
+}
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || "/",
+  base: getBase(),
 });
